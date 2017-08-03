6c8815a940cab68384c055231b51eded0f3a3a36
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-03 19:20:07 +0300

    Added constants and weather class

---
+++ Projects/RainyShinyCloudy/RainyShinyCloudy/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  RainyShinyCloudy
+//
+//  Created by Admin on 8/3/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "26872444cccb1ea9c0ccb7f4fc3da63e"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
---
+++ Projects/RainyShinyCloudy/RainyShinyCloudy/CurrentWeather.swift
@@ -0,0 +1,55 @@
+//
+//  CurrentWeather.swift
+//  RainyShinyCloudy
+//
+//  Created by Admin on 8/3/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class CurrentWeather {
+    var _cityName: String!
+    var _date: String!
+    var _weatherType: String!
+    var _currentTemp: Double!
+    
+    var cityName: String {
+        if _cityName == nil {
+            _cityName = ""
+        }
+        return _cityName
+    } // cityName
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        
+        let currentDate = dateFormatter.string(from: Date())
+        self._date = "Today, \(currentDate)"
+        return _date
+    } // date
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _cityName
+    } // weatherType
+    
+    var currentTemp: Double {
+        if _currentTemp == nil {
+            _currentTemp = 0.0
+        }
+        return _currentTemp
+    } // currentTemp
+    
+    
+    
+} // CurrentWeather
