51dc5b759436f34caa81df1b779596f3c962ebb6
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-07 16:03:55 +0300

    added custom collection view cell

---
+++ Projects/pokedex3/pokedex3/PokeCell.swift
@@ -0,0 +1,27 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Admin on 8/7/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+        
+    }
+    
+}
---
+++ Projects/pokedex3/pokedex3/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  pokedex3
+//
+//  Created by Admin on 8/7/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
