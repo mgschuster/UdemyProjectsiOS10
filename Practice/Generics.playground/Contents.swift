a13e383544523ff930e29e769699adc2b56f5e1f
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-09 16:09:30 +0300

    initial generics commit

---
+++ Practice/Generics.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func intAdder(number: Int) -> Int {
+    return number + 1
+}
+
+intAdder(number: 15)
+
+func doubleAdder(number: Double) -> Double {
+    return number + 1.0
+}
+
+doubleAdder(number: 15.0)
+
+func genericAdder<T: Strideable>(number: T) -> T {
+    return number + 1
+}
+
+
