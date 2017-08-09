0d940fbc01f25b4a4d62575cf13c037a79fb3040
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-09 08:34:01 -0500

    Add files via upload

---
+++ Projects/TacoPOP/TacoPOP/UICollectionView+Ext.swift
@@ -0,0 +1,28 @@
+//
+//  UICollectionView+Ext.swift
+//  TacoPOP
+//
+//  Created by Jack Davis on 7/25/16.
+//  Copyright © 2016 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionView {
+    
+    func register<T: UICollectionViewCell>(_: T.Type) where T: ReusableView, T: NibLoadableView {
+        
+        let nib = UINib(nibName: T.nibName, bundle: nil)
+        register(nib, forCellWithReuseIdentifier: T.reuseIdentifier)
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T where T: ReusableView {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath as IndexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+        }
+        
+        return cell
+    }
+}
+
+extension UICollectionViewCell: ReusableView {}
