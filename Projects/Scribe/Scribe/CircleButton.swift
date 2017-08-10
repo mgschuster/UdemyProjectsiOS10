b50bd078639265285c6580264b47957dc7d46485
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-10 09:44:41 -0500

    Add files via upload

---
+++ Projects/Scribe/Scribe/CircleButton.swift
@@ -0,0 +1,28 @@
+//
+//  CircleButton.swift
+//  Scribe
+//
+//  Created by Admin on 8/10/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 30.0 {
+        didSet {
+            setupView()
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        setupView()
+    }
+    
+    func setupView() {
+        layer.cornerRadius = cornerRadius
+    }
+    
+}
