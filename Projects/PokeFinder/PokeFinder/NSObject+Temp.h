9a8a1e3c9964a139dd9c877c73a92aeff3158704
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-11 19:14:41 +0300

    added FireBase and GeoFinder

---
+++ Projects/PokeFinder/PokeFinder/NSObject+Temp.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+Temp.h
+//  PokeFinder
+//
+//  Created by Admin on 8/11/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Temp)
+
+@end
---
+++ Projects/PokeFinder/PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Admin on 8/11/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+#import "GeoFire.h"
