8a6c1e39242988ac7028e59c6bff11580b5b2b19
Mitchell Schuster <mgschuster11@gmail.com>
2017-08-14 17:51:42 +0300

    added Custom Classes

---
+++ Projects/TJSocial/TJSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  TJSocial
+//
+//  Created by Admin on 8/14/17.
+//  Copyright © 2017 TJSchoost. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
